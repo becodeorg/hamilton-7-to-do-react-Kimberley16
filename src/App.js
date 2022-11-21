c574a7beaa8cb21a66d5319ad778f0717b57d626
Kimberley 16 <kimberleypaladini@gmail.com>
2022-11-21 15:53:11 +0100

    checkbox changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+
 import Todosub from './components/todosub';
 import Button from './components/button';
 import ToDoList from './components/test';
